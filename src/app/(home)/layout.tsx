2883cfc2d2bd9cbfdd0901d6ec09a603e3a3e4f2
kc-clintone <kayseeclintone@gmail.com>
2025-05-15 12:37:59 +0300

    ome layout and structure

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,18 @@
+import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
+import { TRPCProvider } from "@/trpc/client";
+
+interface LayoutProps{
+  children: React.ReactNode
+}
+
+const Layout = ({children}: LayoutProps) => {
+  return (
+    <HomeLayout>
+      <TRPCProvider>
+        {children}
+      </TRPCProvider>
+    </HomeLayout>
+  )
+}
+
+export default Layout
