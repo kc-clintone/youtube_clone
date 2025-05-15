cfe863a09028d4c5696068de233cfbe863a2d69e
kc-clintone <kayseeclintone@gmail.com>
2025-05-15 12:31:53 +0300

    installing packages and loading dependancies

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.mux.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
