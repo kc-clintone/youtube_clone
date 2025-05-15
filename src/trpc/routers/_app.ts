ad5f1909abc7480ee8bfd8f25bfd3a281027807e
kc-clintone <kayseeclintone@gmail.com>
2025-05-15 12:45:48 +0300

    trpc setup

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,12 @@
+import { createTRPCRouter } from '../init';
+import { studioRouter } from '@/modules/studio/server/procedures';
+import { videosRouter } from '@/modules/videos/server/procedures';
+import { categoriesRouter } from '@/modules/categories/server/procedures';
+// import { TRPCError } from '@trpc/server';   
+export const appRouter = createTRPCRouter({
+  studio: studioRouter,
+  videos: videosRouter,
+  categories: categoriesRouter,
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
