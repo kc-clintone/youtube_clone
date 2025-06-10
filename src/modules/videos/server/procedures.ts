1f4304597a1cd712aa6eccd75c43ff163330d03c
kc-clintone <kayseeclintone@gmail.com>
2025-06-10 22:56:24 +0000

    working on video procedure

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,19 +1,43 @@
 import { workflow } from "@/lib/workflow";
 import { db } from "@/db";
-import { videos, videoUpdateSchema } from "@/db/schema";
+import { users, videos, videoUpdateSchema } from "@/db/schema";
 import { mux } from "@/lib/mux";
-import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, eq } from "drizzle-orm";
+import { and, eq, getTableColumns } from "drizzle-orm";
 import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
+  // get one video
+  getOne: baseProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ input }) => {
+      const [existingVideo] = await db
+        .select({
+          ...getTableColumns(videos),
+          user: {
+            ...getTableColumns(users),
+          },
+        })
+        .from(videos)
+        .where(eq(videos.id, input.id))
+        .innerJoin(users, eq(videos.userId, users.id));
+
+      if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return existingVideo;
+    }),
   // generate thumbnail workflow
   generateThumbnail: protectedProcedure
     .input(
       z.object({
-        id: z.string().uuid(), prompt: z.string().min(10),
+        id: z.string().uuid(),
+        prompt: z.string().min(10),
       })
     )
     .mutation(async ({ ctx, input }) => {
