15eb7149b26798227518905a0ef9d4989ee4cd92
kc-clintone <kayseeclintone@gmail.com>
2025-06-11 22:07:41 +0000

    working on video section --<<<prefetching video data>>>

--- src/modules/videos/ui/views/video-view.tsx
+++ src/modules/videos/ui/views/video-view.tsx
@@ -1,3 +1,5 @@
+import { VideoSection } from "../sections/video-section";
+
 interface HomeVideoViewProps {
   videoId: string;
 }
@@ -7,7 +9,7 @@ export const HomeVideoView = ({ videoId }: HomeVideoViewProps) => {
     <div className="mx-auto flex flex-col pt-2 5 mb-10 px-4 mx-w-[1700px]">
       <div className="flex flex-col gap-6 xl:flex-row">
         <div className="flex-1 min-w-0">
-          <VideoSection />
+          <VideoSection videoId={videoId} />
         </div>
       </div>
     </div>
