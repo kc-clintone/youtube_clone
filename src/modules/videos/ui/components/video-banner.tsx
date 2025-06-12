521c75020ee824544248e534b3056dd7df06b526
kc-clintone <kayseeclintone@gmail.com>
2025-06-12 11:28:51 +0000

    homepage video player --- working on video details

--- src/modules/videos/ui/components/video-banner.tsx
+++ src/modules/videos/ui/components/video-banner.tsx
@@ -11,7 +11,9 @@ export const VideoBanner = ({ status }: VideoBannerProps) => {
   return (
     <div className="bg-yellow-500 py-3 px-4 rounded-b-xl items-center gap-2 flex">
       <AlertTriangleIcon className="size-4 text-black shrink-0" />
-      <p>video still processing</p>
+      <p className="text-xs md:text-sm font-medium line-clamp-1 text-black">
+        video still processing
+      </p>
     </div>
   );
 };
