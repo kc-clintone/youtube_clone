305ae02a4e0c00496647a7ceabf6a15525935905
kc-clintone <kayseeclintone@gmail.com>
2025-06-13 09:50:40 +0000

    working on the video player and video details ui --- creating user button variants

--- src/modules/users/ui/components/user-info.tsx
+++ src/modules/users/ui/components/user-info.tsx
@@ -1,3 +1,11 @@
+import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cva, VariantProps } from "class-variance-authority";
+
 const UserInfoVariants = cva("flex items-center gap-1", {
   variants: {
     size: {
@@ -10,3 +18,29 @@ const UserInfoVariants = cva("flex items-center gap-1", {
     size: "default",
   },
 });
+
+interface UserInfoProps extends VariantProps<typeof UserInfoVariants> {
+  name: string;
+  className?: string;
+}
+
+export const UserInfo = ({
+  name,
+  className,
+  size = "default",
+}: UserInfoProps) => {
+  return (
+    <div className={cn(UserInfoVariants({ size, className }))}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <p className="text-gray-500 hover:text-gray-800 line-clamp-1">
+            {name}
+          </p>
+        </TooltipTrigger>
+        <TooltipContent align="center" className="bg-black/70">
+          <p>{name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </div>
+  );
+};
