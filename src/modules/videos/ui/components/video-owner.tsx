73061c10162a69eeeee37e7f7a907ba75f322a6d
kc-clintone <kayseeclintone@gmail.com>
2025-06-13 08:37:10 +0000

    working on the video player and video details ui --- subscriptions button

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -3,6 +3,7 @@ import { UserAvatar } from "@/components/user-avatar";
 import { VideoGetOneOutput } from "@/modules/types";
 import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
+import { SubButton } from "./sub-button";
 
 interface VideoOwnerProps {
   user: VideoGetOneOutput["user"];
@@ -26,7 +27,7 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
           <Link href={`/studio/videos/${videoId}`}>Edit video</Link>
         </Button>
       ) : (
-        <Button>Subscribe</Button>
+        <SubButton />
       )}
     </div>
   );
