570de2fff23ed162d96ea245907d3a627efcecaf
kc-clintone <kayseeclintone@gmail.com>
2025-06-13 09:18:35 +0000

    working on the video player and video details ui --- creating user button variants

---
+++ src/modules/users/ui/components/user-info.tsx
@@ -0,0 +1,12 @@
+const UserInfoVariants = cva("flex items-center gap-1", {
+  variants: {
+    size: {
+      default: "[&_p]:text-sm [&_svg]:size-4",
+      lg: "[&_p]:text-base [&_svg]:size-5 [&_p]:font-medium [&_p]:text-black",
+      sm: "[&_p]:text-xs [&_svg]:size-3",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
