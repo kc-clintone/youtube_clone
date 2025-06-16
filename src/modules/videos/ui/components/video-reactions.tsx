39a808e76c26a2936395fe41174c20e88a9d0eb1
kc-clintone <kayseeclintone@gmail.com>
2025-06-16 08:48:59 +0000

    working on video player components --- video interactivity

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -1,5 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { ThumbsUpIcon, ThumbsDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Seperator } from "@/components/ui/seperator";
+
 export const VideoReactions = () => {
-    return (
-        <div>Like</div>
-    )
-}
+  const VieweroReaction = "like" | "dislike";
+  const likes = 10;
+  const dislikes = 2;
+  return (
+    <div className="flex items-center flex-none">
+      <Button
+        variant="secondary"
+        className="rounded-l-none rounded-r-none gap-2 pr-4"
+      >
+        <ThumbsUpIcon
+          className={cn("size-4", VieweroReaction === "like" && "fill-black")}
+        />
+        {likes}
+      </Button>
+      <Seperator className="h-7" orientation="vertical"/>
+      <Button
+        variant="secondary"
+        className="rounded-l-none rounded-r-full pl-3"
+      >
+        <ThumbsDownIcon
+          className={cn(
+            "size-4",
+            VieweroReaction === "dislike" && "fill-black"
+          )}
+        />
+        <span className="ml-2">{dislikes}</span>
+      </Button>
+    </div>
+  );
+};
