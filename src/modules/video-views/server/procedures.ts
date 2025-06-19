58e7470057d9b5c4b19d18f2c7b310400e67c82c
kc-clintone <kayseeclintone@gmail.com>
2025-06-19 10:11:57 +0000

    working on video views procedure for keeping track of views -- a user can only have one view per video

--- src/modules/video-views/server/procedures.ts
+++ src/modules/video-views/server/procedures.ts
@@ -1,18 +1,38 @@
-import { db } from '@/db';
+import { eq } from "drizzle-orm";
+import { and } from "drizzle-orm";
+import { videoViews } from "./../../../db/schema";
+import { db } from "@/db";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const VideoViewsRouter = createTRPCRouter({
-    create: protectedProcedure
-        .input(z.object({
-            videoId: z.string().uuid(),
-        }))
-        .mutation(async ({ ctx, input}) => {
-            const { videoId } = input;
-            const {id: userId} = ctx.user;
+  create: protectedProcedure
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { videoId } = input;
+      const { id: userId } = ctx.user;
 
-            const existingView = await db
-                .select()
-                .from(videoViews)
+      const existingView = await db
+        .select()
+        .from(videoViews)
+        .where(
+          and(eq(videoViews.userId, userId), eq(videoViews.videoId, videoId))
+        );
+
+      if (existingView) return existingView;
+
+      const [createdView] = await db
+        .insert(videoViews)
+        .values({
+          userId,
+          videoId,
         })
-})
+        .returning();
+
+      return createdView;
+    }),
+});
