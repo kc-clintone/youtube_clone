dae8ca06e3e5e4e38e6a94e98f6eaa8598b92fc8
kc-clintone <kayseeclintone@gmail.com>
2025-06-20 16:19:35 +0000

    refactor: enhance like and dislike mutation handling in VideoReactions component

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -5,6 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import { VideoGetOneOutput } from "@/modules/types";
 import { useClerk } from "@clerk/clerk-react";
 import { trpc } from "@/trpc/client";
+import { toast } from "sonner";
 
 interface VideoReactionsProps {
   viewerReaction: VideoGetOneOutput["viewerReaction"];
@@ -18,16 +19,44 @@ export const VideoReactions = ({
   videoId,
   likes,
   dislikes,
-}:VideoReactionsProps) => {
+}: VideoReactionsProps) => {
   // the logic to handle like and dislike reactions
-  const [clerkUser] = useClerk()
+  const clerkUser = useClerk();
   const utils = trpc.useUtils();
 
-  const likeMutation = trpc.videoReactions.like.useMutation()
-  const dislikeMutation = trpc.videoReactions.dislike.useMutation()
+  const likeMutation = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: invalidate playlists in future
+    },
+    onError: (error) => {
+      toast.error("Somehing wen wrong!");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerkUser.openSignIn();
+      }
+    },
+  });
+
+  const dislikeMutation = trpc.videoReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: invalidate playlists in future
+    },
+    onError: (error) => {
+      toast.error("Somehing wen wrong!");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerkUser.openSignIn();
+      }
+    },
+  });
+
   return (
     <div className="flex items-center flex-none">
       <Button
+        onClick={() => likeMutation.mutate({ videoId })}
+        disabled={likeMutation.isPending || dislikeMutation.isPending}
         variant="secondary"
         className="rounded-l-none rounded-r-none gap-2 pr-4"
       >
@@ -38,6 +67,8 @@ export const VideoReactions = ({
       </Button>
       <Separator className="h-7" orientation="vertical" />
       <Button
+        onClick={() => dislikeMutation.mutate({ videoId })}
+        disabled={likeMutation.isPending || dislikeMutation.isPending}
         variant="secondary"
         className="rounded-l-none rounded-r-full pl-3"
       >
