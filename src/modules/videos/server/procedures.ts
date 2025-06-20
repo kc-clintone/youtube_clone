3ada52ee8e822acf4242ce74b40e08c2f2babcbd
kc-clintone <kayseeclintone@gmail.com>
2025-06-20 09:08:08 +0000

    working on video reactions functionality ---> getting current user to associate with the reaction

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -75,7 +75,7 @@ export const videosRouter = createTRPCRouter({
         .innerJoin(users, eq(videos.userId, users.id))
         .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
         .where(eq(videos.id, input.id))
-        .groupBy(videos.id, users.id, viewerReactions.type);
+        // .groupBy(videos.id, users.id, viewerReactions.type);
 
       if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
 
